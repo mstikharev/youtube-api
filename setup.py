cf57fec412e1c49a9510693214f165b7dd4c37b0
Дмитрий Кузьмин <dmitrykzmn@hotmail.com>
2018-08-28 20:15:14 +0400

    Small update

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name='stikpro-youtube-api',
+    version='1.0',
     url='https://github.com/fn12gl34/youtube-api',
     author='Dmitry Kuzmin',
     author_email='dmitrykzmn@hotmail.com',
-    py_modules=['lib'],
+    py_modules=['stikpro_yt_api'],
     install_requires=[
         'apiclient',
         'google-api-python-client'
