e469963b49a3c6a8b7945bfff5167b286c57c957
Дмитрий Кузьмин <dmitrykzmn@hotmail.com>
2018-08-30 21:56:18 +0400

    Added page token

--- stikpro_yt_api.py
+++ stikpro_yt_api.py
@@ -58,10 +58,10 @@ class YoutubeAPI:
             return None
         return list(map(lambda x: x.get('id'), result.get('items')))
 
-    def get_videos_from_playlist(self, pl_id: str, max_results: int = 25) -> list or None:
+    def get_videos_from_playlist(self, pl_id: str, max_results: int = 25, next_page_token: str=None) -> list or None:
         try:
             result: dict = self.api_client.playlistItems() \
-                .list(part='snippet,contentDetails', maxResults=max_results, playlistId=pl_id).execute().get('items')
+                .list(part='snippet,contentDetails', maxResults=max_results, playlistId=pl_id, pageToken=next_page_token).execute().get('items')
         except Exception as e:
             print('Failed to collect playlists item info: {}'.format(e))
             return None
